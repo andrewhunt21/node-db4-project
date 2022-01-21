482aa0c319f5020a6cd1df46b69303201428dfe2
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-20 21:45:35 -0800

    testing recipe id endpoint is wired

--- api/server.js
+++ api/server.js
@@ -5,6 +5,10 @@ const server = express() // server
 
 server.use(express.json()) // server parse json
 
-server.use('/api/recipes', recipesRouter) // server plugging into router
+server.use('/api/recipes', recipesRouter) // server plugging into router, telling url what path to follow, continues following path on router
+
+server.use('*', (req, res) => {
+    res.json({ api: 'is working' }) // catch all endpoint goes after recipes router so that it catches anything that doesn't have an address for router to send to
+})
 
 module.exports = server // exposing server
